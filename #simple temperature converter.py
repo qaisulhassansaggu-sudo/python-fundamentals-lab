4decde72e81dda9c4a88bfb3a6601bd8d856cbdf
qaisulhassansaggu-sudo <qaisulhassansaggu@gmail.com>
2026-09-13 10:31:21 +0500

    Add files via upload

---
+++ #simple temperature converter.py
@@ -0,0 +1,5 @@
+#simple temperature converter
+print("simple temperature converter")
+celsius=float(input("enter temperature in celsius:"))
+fahrenheit=(celsius*9/5)+32
+print(f"{celsius} is equal to {fahrenheit}")
